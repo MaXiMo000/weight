2f3ddb03679376d00787d8b847039c0b9bdf4d8a
MaXiMo000 <109451965+MaXiMo000@users.noreply.github.com>
2022-07-17 16:08:16 +0530

    Add files via upload

---
+++ as.py
@@ -0,0 +1,18 @@
+weight = input('what is ur weight(in pounds):  ')
+kilograms = 0.45 * int(weight)
+yo = 'weight(in kg) = '
+print(yo + str(kilograms))
+height = input('what is ur height(in cm):  ')
+inch = 0.39 * int(height)
+ui = 'inch = '
+print(ui + str(inch))
+length = input('length:  ')
+breath = input('breath:  ')
+area = int(length) * int(breath)
+kj = 'AREA = '
+print(kj + str(area))
+volume = int(height) * int(area)
+kj = 'VOLUME = '
+print(kj + str(volume))
+ku = 'thank u for filling all my form'
+print(ku)
